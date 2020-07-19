4713c2b05463419e80ecd315928082321921ce10
Kevin Skoglund <kevin@novafabrica.com>
2020-07-19 18:22:07 -0400

    Finish movie 04_06

--- simple_cms/app/helpers/application_helper.rb
+++ simple_cms/app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     render(:partial => 'application/error_messages', :locals => {:object => object})
   end
 
+  def status_tag(boolean, options={})
+    options[:true_text] ||= ''
+    options[:false_text] ||= ''
+
+    if boolean
+      content_tag(:span, options[:true_text], :class => 'status true')
+    else
+      content_tag(:span, options[:false_text], :class => 'status false')
+    end
+  end
+
 end
